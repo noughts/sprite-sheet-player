86f63cce40bab7e54131e1e3fea9787bc0dbd312
Koichi Yamamoto <noughts@gmail.com>
2016-03-01 21:18:53 +0900

    a

---
+++ Assets/SpriteSheetPlayer.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SpriteSheetPlayer : MonoBehaviour {
+
+	public Sprite sprite;
+	SpriteRenderer renderer;
+
+	// Use this for initialization
+	void Start () {
+		renderer = GetComponent<SpriteRenderer> ();
+		print (sprite.rect);
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
