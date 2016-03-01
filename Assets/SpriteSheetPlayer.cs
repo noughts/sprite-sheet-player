d123cc4a180ca0ddce52d9d1eae026445fcc3282
Koichi Yamamoto <noughts@gmail.com>
2016-03-01 22:56:50 +0900

    a

--- Assets/SpriteSheetPlayer.cs
+++ Assets/SpriteSheetPlayer.cs
@@ -20,10 +20,9 @@ public class SpriteSheetPlayer : MonoBehaviour {
 		int len = data.frames.Length;
 		sprites = new Sprite[len];
 		for( int i=0; i<len; i++){
-			Frame frame = data.frames [i];
-			Rect rect = frame.getRect ();
-//			rect = new Rect (0,0,300,300);
-			Vector2 pivot = new Vector2 ( 0,0 );
+			Rect rect = data.getRectAt (i);
+			print ("i="+ i+ ", rect="+ rect);
+			Vector2 pivot = new Vector2 ( 0.5f, 0.5f );
 			Sprite sprite = Sprite.Create (texture, rect, pivot );
 			sprites [i] = sprite;
 		}
@@ -31,6 +30,10 @@ public class SpriteSheetPlayer : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
+//		test ();
+//		return;
+
+		print (currentFrame);
 		Sprite sprite = sprites [currentFrame];
 		spriteRenderer.sprite = sprite;
 		currentFrame++;
@@ -38,21 +41,39 @@ public class SpriteSheetPlayer : MonoBehaviour {
 			currentFrame = 0;
 		}
 	}
+
+	public int x;
+	public int y;
+	public int w = 188;
+	public int h = 302;
+	public float px;
+	public float py;
+	void test(){
+		Rect rect = new Rect (x,y,w,h);
+		Vector2 pivot = new Vector2 (px,py);
+		Sprite sprite = Sprite.Create (texture, rect, pivot );
+		spriteRenderer.sprite = sprite;
+	}
+
 }
 
 [System.Serializable]
 class SpriteSheetData{
 	public Frame[] frames;
+	public Metadata meta;
+
+	public Rect getRectAt(int index){
+		Frame frame = frames[index];
+		MyRect r = frame.frame;
+		int y = meta.size.h - r.h - r.y;
+		return new Rect (r.x, y, r.w, r.h);
+	}
 }
 
 [System.Serializable]
 class Frame{
 	public string filename;
-	public FrameRect frame;
-
-	public Rect getRect(){
-		return frame.getRect ();
-	}
+	public MyRect frame;
 
 	public override string ToString(){
 		return "filename="+ filename + ", frame="+ frame;
@@ -60,7 +81,7 @@ class Frame{
 }
 
 [System.Serializable]
-class FrameRect{
+class MyRect{
 	public int x;
 	public int y;
 	public int h;
@@ -74,3 +95,10 @@ class FrameRect{
 		return "x=" + x + ", y=" + y + ", h=" + h + ", w=" + w;
 	}
 }
+
+
+[System.Serializable]
+class Metadata{
+	public MyRect size;
+}
+
